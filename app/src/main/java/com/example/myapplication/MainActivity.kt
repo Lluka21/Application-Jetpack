d8a69a9db427051e8b4ef3cdd8b1917418549b53
Luka Kartvelishvili <kartvelishvilil21@gmail.com>
2026-05-22 17:54:22 +0400

    TextFields

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.myapplication
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,11 +18,18 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.unit.sp
+import androidx.core.view.WindowCompat
 
 
 class MainActivity :    ComponentActivity() {
@@ -39,36 +46,56 @@ class MainActivity :    ComponentActivity() {
 
 @Composable
 fun LoginScreen(){
+    val view = LocalView.current
 
-    var username by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
+    SideEffect {
+        val window = (view.context as Activity).window
+        WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = true
+    }
+
+    var text by remember { mutableStateOf("")}
+    var password by remember { mutableStateOf("")}
 
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color(0xFF121212))
-            .padding(24.dp),
-        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize(),
+        Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
+
     ) {
-        Text("Login")
-        Spacer(modifier = Modifier.height(16.dp))
-        TextField(
-            value = username,
-            onValueChange = {username = it},
-            placeholder = {Text("username")}
-        )
-        Spacer(modifier = Modifier.height(12.dp))
-        TextField(
-            value = password,
-            onValueChange = {password = it},
-            placeholder = {Text("Password")}
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick={}) {
-            Text("Sign In")
+        Text("Sign In SpeechLab", fontSize = 30.sp)
+        Spacer(Modifier.height(12.dp),)
+        Column(){
+            Text("Email Address")
+            Spacer(Modifier.height(8.dp),)
+            TextField(
+                value = text,
+                onValueChange = {text = it},
+                placeholder = {Text( "Enter your email")},
+                modifier = Modifier.width(350.dp)
+            )
         }
-        Spacer(modifier = Modifier.height(16.dp))
-        Text("Forgot Password")
+
+        Spacer(Modifier.height(12.dp),)
+        Column(){
+            Text("Password")
+            Spacer(Modifier.height(8.dp),)
+            TextField(
+                value = password,
+                onValueChange = {password = it},
+                placeholder = {Text( "Enter your password")},
+                modifier = Modifier.width(350.dp)
+            )
+        }
+
+        Spacer(Modifier.height(12.dp),)
+        Text("Forgot Password?")
+        Button(onClick={}) {
+            Text("Login")
+        }
     }
+
+
+
+
+
 }
