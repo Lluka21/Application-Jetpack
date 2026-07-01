21cb221beace32f6ed17795a0b9d6727de33472f
Luka Kartvelishvili <kartvelishvilil21@gmail.com>
2026-07-01 19:14:34 +0400

    created Button component

--- app/src/main/java/screens/LetsStartButton.kt
+++ app/src/main/java/screens/LetsStartButton.kt
@@ -1,10 +1,15 @@
 package com.example.myapplication
 
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 
 @Composable
-fun ForgotPasswordScreen() {
-    Text("Forgot Password Screen")
+fun LetsStartButton() {
+    Button(onClick = {
+
+    }) {
+        Text("Let's Start")
+    }
 }
