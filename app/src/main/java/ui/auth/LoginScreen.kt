39d9970732c121932b7b1f4e04c2dd7dbc6237ed
Luka Kartvelishvili <kartvelishvilil21@gmail.com>
2026-07-11 14:12:54 +0400

    create login screen

--- app/src/main/java/ui/auth/LoginScreen.kt
+++ app/src/main/java/ui/auth/LoginScreen.kt
@@ -1,10 +1,35 @@
 package ui.auth
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 
 @Composable
 
 fun LoginScreen() {
-    Text("This is Login Screen")
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+
+    ){
+        Text(
+            text = "An easy way to manage your tasks",
+            fontSize = 20.sp,
+            color = Color(0xFF888888)
+        )
+
+    }
+
+
 }
