fd53d15cb4100a8cce9b2d283fdac44f11ea677e
Luka Kartvelishvili <kartvelishvilil21@gmail.com>
2026-07-17 13:18:16 +0400

    AuthViewModel add

--- app/src/main/java/ui/auth/AuthViewModel.kt
+++ app/src/main/java/ui/auth/AuthViewModel.kt
@@ -1,2 +1,24 @@
 package ui.auth
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class AuthViewModel : ViewModel() {
+
+    var email by mutableStateOf("")
+        private set
+
+    var password by mutableStateOf("")
+        private set
+
+
+    fun onEmailChange(newEmail: String) {
+        email = newEmail
+    }
+
+    fun onPasswordChange(newPassword: String) {
+        password = newPassword
+    }
+}
