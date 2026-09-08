0502af313a2580d149cbb18813d5745037c44594
Luka Kartvelishvili <kartvelishvilil21@gmail.com>
2026-09-08 19:39:55 +0400

    Testing backend - validations

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,24 @@ const express = require("express")
 
 const app = express();
 
-app.get("/test",(req, res) => {
+app.use(express.json());
+
+app.post("/auth/signup", (req,res) => {
+    const { username, email, password } = req.body;
+
+    if(!username || !email || !password ) {
+        return res.status(400).json({
+            message: "All fields are required"
+        })
+
     res.json({
-    message: "Backend is working!"
+        username: username,
+        email: email,
+        password: password
     });
-})
 
-app.post("/test",(req,res) => {
-    res.json({
-        message: "POST is working"
-    })
-})
-
+    }
+});
 
 app.listen(3000,() => {
     console.log("Server is running on port 3000")
